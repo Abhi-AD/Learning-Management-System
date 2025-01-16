d1623eaa137434803f868a77292f871efacedfa3
Abhi-AD <dangiabhi332@gmail.com>
2025-01-16 21:48:34 +0545

    backend: Create django models & register in admin

--- backend/apps/main/admin.py
+++ backend/apps/main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from apps.main.models import Teacher, CourseCategory, Course, Student
 
 # Register your models here.
+
+admin.site.register(Teacher)
+admin.site.register(CourseCategory)
+admin.site.register(Course)
+admin.site.register(Student)
